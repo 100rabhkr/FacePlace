23f5574a7f73264d6ce56ebf8efbec93d2653b8f
100rabhkr <skkumar.sk94@gmail.com>
2019-07-02 20:11:34 +0530

    Created a FaceDetector Class which detects face and creates frame. It also adds padding to the images as a walk around for an underlying bug!

---
+++ app/src/main/java/tech/mobisuite/faceplace/FaceActivity.java
@@ -0,0 +1,13 @@
+package tech.mobisuite.faceplace;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FaceActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_face);
+    }
+}
